df997e25acdad29e46c506efba8ec1e13bb9458d
Rishi Singhal <rishisinghal10@gmail.com>
2019-08-16 12:04:03 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
+import Loading from './Loading'
+class App extends React.Component{
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             lat:null,
+            errorMessage:''
+        }
+    }
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+           position=>this.setState({lat:position.coords.latitude}),
+            (err)=>this.setState({errorMessage:"User Denied"})
+        );
+    }
+    helperrender(){
+        if(this.state.lat)
+            return <SeasonDisplay lat={this.state.lat}/>
+
+        if(!this.state.errorMessage)
+            return <Loading message="Please Accept Location Request"/>
+
+        return <h1>{this.state.errorMessage}</h1>
+    }
+    // react says we have to define render
+    render() {
+        return <div className="border red">{this.helperrender()}</div>
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'))
